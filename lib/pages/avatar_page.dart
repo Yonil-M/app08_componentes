3d8980ee37e0d0a64af3a6c32326671b8fdcb1d4
Yonil <201913301c@utea.edu.pe>
2022-10-31 11:11:22 -0500

    carga imagen AvatarP

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -7,9 +7,33 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Avatar Page"),
+        title:const Text("Avatar Page"),
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.purple,
+        actions: [
+          CircleAvatar(
+            backgroundImage:NetworkImage("https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg") ,
+          ),
+          IconButton(onPressed: (){}, 
+          icon:Icon(Icons.more_vert),),
+          ],
       ),
-      body: const Text("Hello"),
-    );
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            FadeInImage(
+              placeholder:AssetImage("assets/images/loading.gif"),
+               image:NetworkImage("https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"),
+               fit: BoxFit.cover,
+               fadeInDuration: Duration(milliseconds: 1300),
+               ),
+        
+          ],
+          
+        ),
+      ),
+      );
+      
   }
 }
